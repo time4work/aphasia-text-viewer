86d187a687a7479b949329f9d5a6b75e8b4368c6
Nik Lazariev <nlazariev@softjourn.com>
2019-05-27 01:13:56 +0200

    nav-bar + empty lib components

--- src/app/components/nav-bar/nav-bar.component.ts
+++ src/app/components/nav-bar/nav-bar.component.ts
@@ -1,12 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
-  selector: 'app-nav-bar',
+  selector: 'nav-bar',
   templateUrl: './nav-bar.component.html',
   styleUrls: ['./nav-bar.component.scss']
 })
 export class NavBarComponent implements OnInit {
 
+  @Input() title: string;
+  
   constructor() { }
 
   ngOnInit() {
